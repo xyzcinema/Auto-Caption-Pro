403d69e139576eb2013d658abc1f1a72ce4b7ace
terabox-player <xyzcinema4@gmail.com>
2026-03-01 18:32:14 +0545

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -7,8 +7,9 @@ from plugins.start import router as start_router
 from plugins.settings import router as settings_router
 from plugins.video import router as video_router
 from plugins.admin import router as admin_router
+from plugins.caption import router as caption_router
 
-__all__ = ["start_router", "settings_router", "video_router", "admin_router"]
+__all__ = ["start_router", "settings_router", "video_router", "admin_router", "caption_router"]
 # CantarellaBots
 # Don't Remove Credit
 # Telegram Channel @CantarellaBots
