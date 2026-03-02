83d844ca6f6c705539ce768780e63bbfc85727f4
terabox-player <xyzcinema4@gmail.com>
2026-03-02 08:22:05 +0545

    Update config.py

--- config.py
+++ config.py
@@ -4,54 +4,26 @@
 #Supoort group @rexbotschat
 import os
 import random
-# CantarellaBots
-# Don't Remove Credit
-# Telegram Channel @CantarellaBots
-#Supoort group @rexbotschat
-# Bot Configuration
-API_TOKEN = os.environ.get("API_TOKEN", "")
-# CantarellaBots
-# Don't Remove Credit
-# Telegram Channel @CantarellaBots
-#Supoort group @rexbotschat
-# MongoDB
-MONGO_URL = os.environ.get("MONGO_URL", "")
+
+API_TOKEN = os.environ.get("API_TOKEN", "") #required
+
+
+MONGO_URL = os.environ.get("MONGO_URL", "") #required
 DB_NAME = "thumbnail_bot"
-# CantarellaBots
-# Don't Remove Credit
-# Telegram Channel @CantarellaBots
-#Supoort group @rexbotschat
-# Owner/Admin
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-# CantarellaBots
-# Don't Remove Credit
-# Telegram Channel @CantarellaBots
-#Supoort group @rexbotschat
+OWNER_ID = int(os.environ.get("OWNER_ID", "")) # Owner/Admin
 # UI URLs - Multiple images that rotate randomly
 # Use DIRECT image URLs (https://i.ibb.co/...) not page URLs (https://ibb.co/...)
 START_PICS = [
     "https://i.ibb.co/0jjgxKM4/changli-wuthering-waves-4k-wallpaper-uhdpaper-com-437-2-b.jpg",
     # Add more direct image URLs here
 ]
-# CantarellaBots
-# Don't Remove Credit
-# Telegram Channel @CantarellaBots
-#Supoort group @rexbotschat
-CHANNEL_URL = os.environ.get("CHANNEL_URL", "https://t.me/cantarellabots")
-DEV_URL = os.environ.get("DEV_URL", "https://t.me/cantarella_wuwa")
+CHANNEL_URL = os.environ.get("CHANNEL_URL", "https://t.me/Sujan_BotZ")
+DEV_URL = os.environ.get("DEV_URL", "https://t.me/Sujan_Ch")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))  # e.g., -100xxxxxxxxxxxx
-# CantarellaBots
-# Don't Remove Credit
-# Telegram Channel @CantarellaBots
-#Supoort group @rexbotschat
 
 def get_random_pic() -> str:
     """Get a random image from START_PICS."""
     if START_PICS:
         return random.choice(START_PICS)
     return None
-# CantarellaBots
-# Don't Remove Credit
-# Telegram Channel @CantarellaBots
-#Supoort group @rexbotschat
 
