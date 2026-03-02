96cf458e801ebe0304778ebd8e305b7a994076bd
terabox-player <xyzcinema4@gmail.com>
2026-03-02 08:26:51 +0545

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,8 +1,3 @@
-# CantarellaBots
-# Don't Remove Credit
-# Telegram Channel @CantarellaBots
-#Supoort group @rexbotschat
-
 from plugins.start import router as start_router
 from plugins.settings import router as settings_router
 from plugins.video import router as video_router
@@ -10,7 +5,3 @@ from plugins.admin import router as admin_router
 from plugins.caption import router as caption_router
 
 __all__ = ["start_router", "settings_router", "video_router", "admin_router", "caption_router"]
-# CantarellaBots
-# Don't Remove Credit
-# Telegram Channel @CantarellaBots
-#Supoort group @rexbotschat
